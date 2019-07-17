def0ba2d7b7ab15334180e3b95fb4869de81ebb7
Kelly Salamack <kellysalamackdesign@gmail.com>
2019-07-17 17:53:21 -0400

    added orm and connection

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+//global
+const mysql = require("mysql");
+
+//create connection
+const connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        root: 3000,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: "burgers_db"
+    });
+};
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+//export connection
+module.exports = connection;
---
+++ server.js
@@ -0,0 +1,8 @@
+//dependencies
+
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
